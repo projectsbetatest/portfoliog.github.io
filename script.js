5ab5dbb1db7cdd1e587a6ab79c95d3519acb113b
projectsbetatest <levserst@gmail.com>
2026-05-24 20:42:55 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Меняем цвет фона при нажатии на меню
-document.querySelectorAll('.header nav a').forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.style.backgroundColor = '#f0f0f0';
-    });
-});
